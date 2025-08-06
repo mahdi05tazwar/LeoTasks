19a97702246ab1abb34e0d971d124ab7abb3d0f2
Gábor Nyékhelyi <gabor@wearedip.eu>
2025-08-06 23:18:27 +0200

    added sqlite maven dependency, updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.mtr.leotasks {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires sqlite.jdbc;
-
 
     opens com.mtr.leotasks to javafx.fxml;
     exports com.mtr.leotasks;
